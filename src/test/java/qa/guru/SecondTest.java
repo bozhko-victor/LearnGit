5902cdbb958e7388d50c45604f3bfae65ac9b5ad
bozhko-victor <bozhko.v.n@yandex.ru>
2021-09-26 17:58:40 +0300

    Added SecondTest

---
+++ src/test/java/qa/guru/SecondTest.java
@@ -0,0 +1,13 @@
+package qa.guru;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SecondTest {
+
+    @Test
+    void secondTest() {
+        Assertions.assertTrue(true);
+    }
+
+}
